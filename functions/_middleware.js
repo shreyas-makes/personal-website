a828baa59014cef7ff8183d401927584cfbc3398
Shreyas <shreyas314159@gmail.com>
2025-04-28 22:19:50 +0100

    removed wrangler

--- functions/_middleware.js
+++
@@ -1,8 +0,0 @@
-// This middleware runs before all functions
-export async function onRequest(context) {
-  // Extract request and env
-  const { request, env, next } = context;
-  
-  // For all routes, continue without modification
-  return next();
-} 
