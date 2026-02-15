3f549501e3fbda3c183b29e69fc1f6e0e7ab2b6e
Shreyas <shreyas314159@gmail.com>
2026-02-15 20:31:22 +0000

    removed philosophers section from the blog

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface Window {
+  mermaid?: any;
+  marked?: any;
+  Prism?: any;
+  toggleSearch?: () => void;
+}
+
+declare var mermaid: any;
+declare var marked: any;
+declare var Prism: any;
